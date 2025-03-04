a51e91d0a0ca13fa008c90b4d566864aa254b1d2
MoiseFlori <moiseflori@outlook.com>
2025-03-04 21:04:41 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const basename = "/Personal-Website"; // Numele repo-ului GitHub
+
+export default function Root() {
+  return (
+    <StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
